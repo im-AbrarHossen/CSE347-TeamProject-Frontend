26cb1877df51249f606b5e05b0a0c9e1800334aa
Abrar <hossainabrar470@gmail.com>
2025-08-20 00:18:03 +0600

    tailwind css and daisyui install and setup

--- src/routers/router.jsx
+++ src/routers/router.jsx
@@ -1,11 +1,16 @@
-import {
-    createBrowserRouter,
-} from "react-router-dom";
+import { createBrowserRouter } from "react-router-dom";
+
+import Home from "../pages/public-pages/Home";
+import About from "../pages/public-pages/About";
 
 const router = createBrowserRouter([
     {
         path: "/",
-        
+        element: <Home></Home>
     },
+    {
+        path: "/about",
+        element: <About></About>
+    }
 ]);
 export default router;
